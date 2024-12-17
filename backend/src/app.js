0e91191988f1205921bbbe666206bb06ddc343e8
Manoj Kumar <manojkumarcse432@gmail.com>
2024-12-17 22:56:55 +0530

    added-vercel.json-file

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://firstwin.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
