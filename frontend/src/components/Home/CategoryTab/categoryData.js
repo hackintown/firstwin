4564bc8363f2ebc3261ff5963a1da8a18a4d3e42
manoj mandal <hackintown99@gmail.com>
2024-12-19 18:20:54 +0530

    category-tab-added

---
+++ frontend/src/components/Home/CategoryTab/categoryData.js
@@ -0,0 +1,53 @@
+
+export const categoryItems = [
+  {
+    id: 1,
+    title: "Super Jackpot",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150522p3ab.png"
+  },
+  {
+    id: 2,
+    title: "Lottery",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  },
+  {
+    id: 3,
+    title: "Original",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150154dt6o.png"
+  },
+  {
+    id: 4,
+    title: "Slots",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150258cvmo.png"
+  },
+  {
+    id: 5,
+    title: "Books",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  },
+  {
+    id: 6,
+    title: "Beauty",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  },
+  {
+    id: 7,
+    title: "Toys",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  },
+  {
+    id: 8,
+    title: "Automotive",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  },
+  {
+    id: 9,
+    title: "Health",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  },
+  {
+    id: 10,
+    title: "Groceries",
+    imageUrl: "https://ossimg.dmwinimage.com/DmWin/gamecategory/gamecategory_20241011150240v69r.png"
+  }
+] 
--- frontend/src/pages/User/Dashboard.jsx
+++ frontend/src/pages/User/Dashboard.jsx
@@ -3,11 +3,13 @@ import Banner from "../../components/User/Dashboard/Banner";
 import NoticeBar from "../../components/User/Dashboard/NoticeBar";
 import Lottery from "../../components/Home/Lottery/Lottery";
 import Popular from "../../components/Home/Popular/Popular";
+import CategoryTab from "../../components/Home/CategoryTab/CategoryTab";
 const Dashboard = () => {
   return (
     <div className="flex flex-col py-4 space-y-6">
       <Banner />
       <NoticeBar />
+      <CategoryTab />
       <Lottery />
       <Popular />
     </div>
